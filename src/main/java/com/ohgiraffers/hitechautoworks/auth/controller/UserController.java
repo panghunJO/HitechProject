5357cbe8fb09813101d6d0ce1cb5ea285aeee1f1
DESKTOP-DP3RJ5I\hi <sunnyskylove22@gmail.com>
2024-05-07 17:38:24 +0900

    [24.05.07] UserList 조회

--- src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.ohgiraffers.hitechautoworks.auth.controller;
 
+import com.ohgiraffers.hitechautoworks.auth.dto.RepairDTO;
+import com.ohgiraffers.hitechautoworks.auth.dto.UserDTO;
+import com.ohgiraffers.hitechautoworks.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/user/dashboard")
     public void dashboard(Model model) {
@@ -31,5 +39,31 @@ public class UserController {
     @GetMapping("/employee/part/partAdd")
     public void partAdd(){}
 
+//    @GetMapping("/employee/repair/repair")
+//    public void repair(){}
+//
+//    @PostMapping("/employee/repair/repair")
+//    public String repair(@RequestParam String userName, @RequestParam int userCode){
+//        System.out.println("userName = " + userName);
+//        System.out.println("userCode = " + userCode);
+//
+//        List<RepairDTO> repairList = UserService.findRepairInfo();
+//
+//
+//        return "/employee/repair/repair";
+//    }
+    @GetMapping("/employee/account/account")
+    public String account(Model model){
+        List<UserDTO> userList = userService.findAllUser();
+        System.out.println("userList = " + userList);
+        model.addAttribute("userList", userList);
+        return "employee/account/account";
 
-}
+    }
+
+
+
+
+    }
+
+
