232a9e1e25027f5215028fcae5c75fc74dd08165
yoonjae <realyoon77@gmail.com>
2024-05-07 17:43:11 +0900

    [2024-05-07] 코드로 올리기

--- src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.ohgiraffers.hitechautoworks.auth.controller;
 
+import com.ohgiraffers.hitechautoworks.auth.dto.PartDTO;
+import com.ohgiraffers.hitechautoworks.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/dashboard")
     public void dashboard(Model model) {
@@ -26,6 +38,8 @@ public class UserController {
     public void part2(@RequestParam String partName, @RequestParam int partCode){
         System.out.println("partName = " + partName);
         System.out.println("partCode = " + partCode);
+        List<PartDTO> partList = userService.selectPartByCode(partCode);
+        System.out.println("partList = " + partList);
     }
 
     @GetMapping("/employee/part/partAdd")
