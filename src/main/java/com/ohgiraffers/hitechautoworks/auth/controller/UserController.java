8e452d53ac333aa11d64a19534b418ecc2ff2695
yoonjae <realyoon77@gmail.com>
2024-05-10 16:27:40 +0900

    [2024-05-10] 최종수정

--- src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/UserController.java
@@ -10,11 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
     private AuthUserInfo authUserInfo;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/user/dashboard")
     public void dashboard(Model model) {
@@ -66,8 +71,6 @@ public class UserController {
             userService.updateUser(userId, userPw, userEmail, userPwCheck, userAddress, userPhone, userName);
             return "customer/res/res";
         }
-
-
 
     }
 }
