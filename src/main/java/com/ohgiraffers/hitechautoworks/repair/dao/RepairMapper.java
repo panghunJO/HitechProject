c9d0a3c6bb2a8b594cb7dd8f01463c55c68c7657
jieun <ggeun1031@naver.com>
2024-05-13 23:14:37 +0900

    [2024-05-13] repair 검색, 상세보기, 수정, 삭제 , 등록하기

--- src/main/java/com/ohgiraffers/hitechautoworks/repair/dao/RepairMapper.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/repair/dao/RepairMapper.java
@@ -1,14 +1,47 @@
 package com.ohgiraffers.hitechautoworks.repair.dao;
 
+import com.ohgiraffers.hitechautoworks.auth.dto.UserDTO;
+import com.ohgiraffers.hitechautoworks.part.dto.PartDTO;
+import com.ohgiraffers.hitechautoworks.repair.dto.Repair2DTO;
 import com.ohgiraffers.hitechautoworks.repair.dto.RepairDTO;
-import com.ohgiraffers.hitechautoworks.repair.dto.RepairPartDTO;
+import com.ohgiraffers.hitechautoworks.res.dto.ResDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Date;
 import java.util.List;
 
 @Mapper
 public interface RepairMapper {
 
     List<RepairDTO> findAllRepair();
-    List<RepairPartDTO> findRepairPart();
+    List<RepairDTO> SearchByworkerCode(int workerCode);
+
+    List<RepairDTO> SearchByworkerName(String worker);
+
+    Repair2DTO selectRepair(int resCode);
+
+    void modifyRepair(int resCode, String content, String status, Date date);
+
+    int selectUserCodeByUserName(String userName);
+
+    int selectPartCodeByPartName(String partName);
+
+    void modifyRepairWorker(int newUserCode, int resCode);
+
+    void modifyRepairPart(int newPartCode, int resCode);
+
+    void deleteRepair(int resCode);
+
+    List<PartDTO> findPartList();
+
+    List<UserDTO> findWorkerList();
+
+    List<ResDTO> findResList();
+
+    void addRepair(int resCode, String content, String status, Date date);
+
+    void addRepairPart(int newPartCode, int resCode);
+
+    void addRepairWorker(int newUserCode, int resCode);
+
 }
--- src/main/java/com/ohgiraffers/hitechautoworks/repair/service/RepairService.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/repair/service/RepairService.java
@@ -1,10 +1,15 @@
 package com.ohgiraffers.hitechautoworks.repair.service;
 
+import com.ohgiraffers.hitechautoworks.auth.dto.UserDTO;
+import com.ohgiraffers.hitechautoworks.part.dto.PartDTO;
 import com.ohgiraffers.hitechautoworks.repair.dao.RepairMapper;
+import com.ohgiraffers.hitechautoworks.repair.dto.Repair2DTO;
 import com.ohgiraffers.hitechautoworks.repair.dto.RepairDTO;
+import com.ohgiraffers.hitechautoworks.res.dto.ResDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 @Service
 public class RepairService {
@@ -15,9 +20,62 @@ public class RepairService {
     public List<RepairDTO> findAllRepair() {
         return repairMapper.findAllRepair();
     }
-    //    public List<RepairDTO> findRepairInfo() {
-//
-//
-//        return repairMapper.findRepairInfo();
-//    }
+
+    public List<RepairDTO> SearchByworkerCode(int workerCode) {
+        return repairMapper.SearchByworkerCode(workerCode);
+    }
+
+    public List<RepairDTO> SearchByworkerName(String worker) {
+        return repairMapper.SearchByworkerName(worker);
+    }
+
+    public Repair2DTO selectRepair(int resCode) {
+        return repairMapper.selectRepair(resCode);
+    }
+
+    public void modifyRepair(int resCode, String content, String status, Date date) {
+        repairMapper.modifyRepair(resCode, content, status, date);
+    }
+
+    public void modifyRepairWorker(String userName, int resCode) {
+        int newUserCode = repairMapper.selectUserCodeByUserName(userName);
+        repairMapper.modifyRepairWorker(newUserCode, resCode);
+    }
+
+    public void modifyRepairPart(String partName, int resCode) {
+        int newPartCode = repairMapper.selectPartCodeByPartName(partName);
+        repairMapper.modifyRepairPart(newPartCode, resCode);
+    }
+
+    public void deleteRepair(int resCode) {
+        repairMapper.deleteRepair(resCode);
+    }
+
+    public List<PartDTO> findPartList() {
+        return repairMapper.findPartList();
+    }
+
+    public List<UserDTO> findWorkerList() {
+        return repairMapper.findWorkerList();
+    }
+
+    public void addRepair(int resCode, String content, String status, Date date) {
+        repairMapper.addRepair(resCode,content,status,date);
+    }
+
+    public void addRepairPart(String partName, int resCode) {
+        System.out.println("partName = " + partName);
+        int newPartCode = repairMapper.selectPartCodeByPartName(partName);
+        repairMapper.addRepairPart(newPartCode,resCode);
+    }
+
+    public void addRepairWorker(String userName, int resCode) {
+        System.out.println("userName = " + userName);
+        int newUserCode = repairMapper.selectUserCodeByUserName(userName);
+        repairMapper.addRepairWorker(newUserCode,resCode);
+    }
+
+    public List<ResDTO> findResList() {
+        return repairMapper.findResList();
+    }
 }
