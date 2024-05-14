dbb3827689f473ad866cb29b10bac424825f3d83
DESKTOP-DP3RJ5I\hi <sunnyskylove22@gmail.com>
2024-05-14 17:00:31 +0900

    Admin_AccountModifyDelete

--- src/main/java/com/ohgiraffers/hitechautoworks/account/controller/AccountController.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.ohgiraffers.hitechautoworks.account.service.AccountService;
 import com.ohgiraffers.hitechautoworks.auth.dto.UserDTO;
 import com.ohgiraffers.hitechautoworks.auth.service.Details.AuthUserInfo;
 import com.ohgiraffers.hitechautoworks.auth.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -67,6 +68,51 @@ public class AccountController {
         model.addAttribute("userName",userName);
         return "admin/account/account";
     }
+
+    // 수정
+    @GetMapping("/admin/account/AccountModify")
+    public void AccountModify(@RequestParam int userCode, Model model, HttpSession session) {
+
+        System.out.println("userCode = " + userCode);
+        UserDTO userDTO = accountService.selectAccount(userCode);
+        System.out.println("userDTO = " + userDTO);
+        model.addAttribute("userDTO", userDTO);
+        authUserInfo = new AuthUserInfo();
+        UserDTO userDTO1 = authUserInfo.getUserDTO();
+        String userName = userDTO1.getUserName();
+        model.addAttribute("userName",userName);
+    }
+
+    @PostMapping("/admin/account/AccountModify")
+    public String account(@RequestParam String userId, @RequestParam int userCode,@RequestParam String userDepartment, @RequestParam String userPw, @RequestParam String userPwCheck, @RequestParam String userName,
+                          @RequestParam String userEmail, @RequestParam String userAddress, @RequestParam String userPhone, Model model,
+                          HttpSession session) {
+        System.out.println("userId = " + userId);
+        System.out.println("userPw = " + userPw);
+        System.out.println("userPwCheck = " + userPwCheck);
+        System.out.println("userDepartment = " + userDepartment);
+        accountService.updateUser(userId, userCode, userDepartment, userPw, userEmail, userPwCheck, userAddress, userPhone, userName);
+        if (!userPw.equals(userPwCheck)) {
+            // 비밀번호 확인이 일치하지 않을 경우 에러 메시지 전달하고 리다이렉트
+            session.setAttribute("errorMessage","비밀번호가 일치하지 않습니다.");
+            return "redirect:/admin/account/account";
+
+        } else {
+            accountService.updateUser(userId, userCode, userDepartment, userPw, userEmail, userPwCheck, userAddress, userPhone, userName);
+            return "redirect:/admin/account/account";
+        }
+    }
+
+    @PostMapping("/admin/account/AccountDelete")
+    public String accountDeleteUser(@RequestParam int userCode) {
+        authUserInfo = new AuthUserInfo();
+        UserDTO userDTO = authUserInfo.getUserDTO();
+        accountService.accountDeleteUser(userCode);
+
+        return "redirect:/admin/account/account";
+
+    }
+
 
 
 }
