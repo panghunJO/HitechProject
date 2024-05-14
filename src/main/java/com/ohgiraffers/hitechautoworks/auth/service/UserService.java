f76de94759093d9995f85605034a200fb1f42c05
yoonjae <realyoon77@gmail.com>
2024-05-15 00:00:49 +0900

    설정 수정

--- src/main/java/com/ohgiraffers/hitechautoworks/auth/service/UserService.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/auth/service/UserService.java
@@ -28,7 +28,6 @@ public class UserService  {
 
     public String overlappedID(String id) {
         String result = userMapper.overlappedID(id);
-        System.out.println("result = " + result);
         return result;
     }
 
