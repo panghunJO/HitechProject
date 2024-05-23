e925cc400e7faaee9b0ce620963b0179d838a872
yoonjae <realyoon77@gmail.com>
2024-05-23 16:58:43 +0900

    [2024-05-23] 아이디/비밀번호 찾기 추가 부품 등록 페이지 수정

--- src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/AuthController.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/auth/controller/AuthController.java
@@ -12,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
@@ -35,5 +38,31 @@ public class AuthController {
             result1 = 0;
         }
         return result1; // int 값을 String으로 변환하여 반환
+    }
+
+    @PostMapping("/searchForId")
+    @ResponseBody
+    public Map<String,Object> searchForId(@RequestBody Map<String,Object> info) {
+
+        Map<String,Object> stat = userService.searchForId(info.get("email"));
+
+        if (stat == null) {
+            stat = new HashMap<>();
+        }
+        System.out.println("stat = " + stat);
+
+        return stat;
+    }
+
+    @PostMapping("/searchForPW")
+    @ResponseBody
+    public Map<String,Object> searchForPW(@RequestBody Map<String,Object> info) {
+
+        Map<String,Object> stat = userService.searchForPW(info);
+        if (stat == null) {
+            stat = new HashMap<>();
+        }
+
+        return stat;
     }
 }
