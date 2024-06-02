709e52ebeb14ae5b7ff4851989d02d80eea9829e
yoonjae <realyoon77@gmail.com>
2024-06-02 16:37:02 +0900

    [2024-06-02] 최종

--- src/main/java/com/ohgiraffers/hitechautoworks/config/GlobalExceptionHandler.java
+++ src/main/java/com/ohgiraffers/hitechautoworks/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.hitechautoworks.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +10,18 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoResourceFoundException.class)
     public String handleNoResourceFoundException(NoResourceFoundException exception) {
-        System.out.println("exception = " + exception);
+        log.error("404 exception= {} ", exception);
+        return "error/404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception exception) {
+        log.error("exception= {} ", exception);
         return "error/404";
     }
 }
